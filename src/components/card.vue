c7ce55b95fb72e656e8e86dce56a596f49cc4842
Marc <msamarra@gmail.com>
2023-06-28 18:58:41 +0200

    mostrar imatges pokemons

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,8 +1,11 @@
 <template id="card-template">
-    <article id="" class="card">
-        <h4 class='card-title'></h4>
+    <div class="pokemon">
+        <h4 class='card-title'>NOM</h4>
         <img loading="lazy" alt="" src="" class='card-image' />
         <button class='card-button' onclick="">Detalls</button>
-    </article>
-  </template>
+    </div>
+</template>
+<style>
+
+</style>
   
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,23 +19,57 @@ console.log(pokemons);
 </script>
 
 
-
-
 <template>
   <main>
     <h1>Pokemons</h1>
 
-    <div v-for="pokemon in pokemons">
-      <p>{{ pokemon.name }}</p>
-    </div>
-
-
     <section id="cards" class="cards" margin="10px">
        
-      <div id="view" class='card-list' padding="4px"></div>
+      <div id="view" class='card-list' padding="4px">
+
+        <div class="card" v-for="pokemon in pokemons">
+    
+     
+          <div class="pokemon">
+              <h4 class='card-title'>{{ pokemon.name }}</h4>
+              <img loading="lazy" v-bind:src="pokemon.sprites.front_default">
+
+              
+              <RouterLink to="/details">Detalls</RouterLink>
+          </div>
+
+          
+    
+      </div>
+
+    </div>
 
    </section>
-   <RouterLink to="/details">Detalls</RouterLink>
+   
   </main>
 </template>
+<style>
+.card-list {
+	display: flex;
+	min-height: 100vh;
+	min-width: 200vh;
+	grid-template-columns: repeat(auto-fit, minmax(var(--gridList-column), 1fr));
+	grid-auto-rows: var(--gridList-rows);
+	grid-template-rows: masonry;
+	grid-auto-flow: row dense;
+	grid-gap: var(--gridList-gap, 6px);
+	gap: var(--gridList-gap, 6px);
+	align-items:flex-start;
+}
+
+.card {
+	padding: 10px;
+	box-shadow: inset;
+	width: fit-content;
+	background-color: azure;
+	margin-bottom: 0.5em;
+	position: relative;
+}
+</style>
+
 
